d9914cdc4dd4eaf8b90bd73a70bf813ac1b3699e
Mirko Pavlinović <MirkoP@teched.hr>
2024-10-09 16:14:56 +0200

    datatable init

--- ArchaeoTrack/wwwroot/js/site.js
+++ ArchaeoTrack/wwwroot/js/site.js
@@ -1,7 +1,8 @@
 ﻿window.initializeDataTable = function () {
     $(document).ready(function () {
         $('#notesTable').DataTable({
-            "order": [[0, "desc"]]  // Sort by the first column (Date) in descending order by default
+            "order": [[0, "desc"]]
+            // Sort by the first column (Date) in descending order by default
         });
     });
 };
