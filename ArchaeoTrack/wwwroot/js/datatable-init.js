c13bf7cb78b53656e3b635c3ac8ae75b021eb81f
Mirko Pavlinović <MirkoP@teched.hr>
2024-10-10 16:41:36 +0200

    table fix

---
+++ ArchaeoTrack/wwwroot/js/datatable-init.js
@@ -0,0 +1,15 @@
+﻿window.initializeDataTableIfExists = function (tableSelector) {
+    var tableElement = document.querySelector(tableSelector);
+    if (tableElement && tableElement.rows.length > 1) { // Check if the table has more than one row (i.e., headers + data)
+        var initialized = sessionStorage.getItem('dataTableInitialized');
+        if (initialized !== 'true') {
+            $(document).ready(function () {
+                $(tableSelector).DataTable({
+                    "order": [[0, "desc"]]
+                });
+            });
+            sessionStorage.setItem('dataTableInitialized', 'true');
+        }
+    }
+};
+
--- ArchaeoTrack/wwwroot/js/site.js
+++
@@ -1,8 +0,0 @@
-﻿window.initializeDataTable = function () {
-    $(document).ready(function () {
-        $('#notesTable').DataTable({
-            "order": [[0, "desc"]]
-            // Sort by the first column (Date) in descending order by default
-        });
-    });
-};
