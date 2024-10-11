66c6f7d8b49dbbeb0d2f5219ece773ac954950a7
Mirko Pavlinović <MirkoP@teched.hr>
2024-10-11 12:13:02 +0200

    datatable and waether api

--- ArchaeoTrack/wwwroot/js/datatable-init.js
+++ ArchaeoTrack/wwwroot/js/datatable-init.js
@@ -1,15 +1,36 @@
 ﻿window.initializeDataTableIfExists = function (tableSelector) {
-    var tableElement = document.querySelector(tableSelector);
-    if (tableElement && tableElement.rows.length > 1) { // Check if the table has more than one row (i.e., headers + data)
-        var initialized = sessionStorage.getItem('dataTableInitialized');
-        if (initialized !== 'true') {
-            $(document).ready(function () {
-                $(tableSelector).DataTable({
-                    "order": [[0, "desc"]]
-                });
-            });
-            sessionStorage.setItem('dataTableInitialized', 'true');
+    //var tableElement = document.querySelector(tableSelector);
+    //if (tableElement && tableElement.rows.length > 1) { // Check if the table has more than one row (i.e., headers + data)
+    //    var initialized = sessionStorage.getItem('dataTableInitialized');
+    //    if (initialized !== 'true') {
+    //        $(document).ready(function () {
+    //            $(tableSelector).DataTable({
+    //                "order": [[0, "desc"]]
+    //            });
+    //        });
+    //        sessionStorage.setItem('dataTableInitialized', 'true');
+    //    }
+    //}
+    const table = new simpleDatatables.DataTable(tableSelector, {
+        perPage: 10,
+        searchable: true,
+        sortable: true,
+        deferRender: true, // If supported or consider similar options
+        labels: {
+            placeholder: "Pretraži",
+            searchTitle: "Pretraži unutar tablice",
+            pageTitle: "Stranica {page}",
+            perPage: "zapisa po stranici",
+            noRows: "Nisu pronađeni zapisi",
+            info: "Prikaz {start} do {end} od {rows} zapisa",
+            noResults: "Nema rezulatata pretrage",
         }
-    }
+    });
 };
+
+function TestDataTablesRemove(table) {
+    $(document).ready(function () {
+        $(table).DataTable().destroy();
+    });
+}
 
